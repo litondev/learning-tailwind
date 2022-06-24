d6a804bed5613c3fab912a2d4f4f4d70b4ba7e31
Litoninot <litoninot12345@gmail.com>
2022-06-24 09:14:13 +0700

    add dark mode and mobile signin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}","./asset/*.html"],
+  darkMode: 'class',
   theme: {
     extend: {
       gridTemplateRows: {      
